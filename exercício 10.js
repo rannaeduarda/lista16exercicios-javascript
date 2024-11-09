56fd47ffde6bd38d1c1c7ba18b3c5e3dcc26f496
ranna <rannaeduarda13@gmail.com>
2024-11-08 21:29:27 -0300

    exercício 10.js

---
+++ exercício 10.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+</head>
+<body>
+    <script>
+     let velocidadeMS = parseFloat(prompt("Digite a velocidade em m/s: "));
+     let velocidadeKMH = velocidadeMS * 3.6;
+         console.log("A velocidade em km/h é: " + velocidadeKMH.toFixed(2) + " km/h");
+    </script>
+</body>
+</html>
