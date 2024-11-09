001de2cd1f678b83b876f93ba020f875d6826663
ranna <rannaeduarda13@gmail.com>
2024-11-08 21:27:45 -0300

    exercício 9.js

---
+++ exercício 9.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <script>
+     let anos = parseInt(prompt("Digite a sua idade:\nAnos: "));
+     let meses = parseInt(prompt("Meses: "));
+     let dias = parseInt(prompt("Dias: "));
+     let totalDias = (anos * 365) + (meses * 30) + dias;
+         console.log("A sua idade em dias é: " + totalDias);
+    </script>
+</body>
+</html>
